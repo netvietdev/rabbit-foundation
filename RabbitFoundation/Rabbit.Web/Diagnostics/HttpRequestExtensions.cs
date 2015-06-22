732e5563fa6abe8ee817e6c85c5a9aa88f043afd
Huan HOANG <huanhvhd@gmail.com>
2015-06-22 18:23:55 +0700

    Vo

---
+++ RabbitFoundation/Rabbit.Web/Diagnostics/HttpRequestExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+
+namespace Rabbit.Web.Diagnostics
+{
+    public static class HttpRequestExtensions
+    {
+        public static string GetRequestInfo(this HttpRequest request)
+        {
+            var sb = new StringBuilder();
+            sb.AppendFormat("RawUrl: {0}. HttpMethod: {1}", request.RawUrl, request.HttpMethod);
+
+            if (!request.HttpMethod.Equals(HttpMethod.Get.ToString(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                request.InputStream.Position = 0;
+                var input = new StreamReader(request.InputStream).ReadToEnd();
+
+                sb.AppendLine();
+                sb.AppendFormat("InputStream: {0}", input);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
