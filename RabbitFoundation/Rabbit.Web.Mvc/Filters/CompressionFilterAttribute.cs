a86e85aee3aa622f33b25e7859fa06fae4a69c65
juanonsoftware <juanonsoftware@github.com>
2015-07-21 19:28:11 +0700

    Add new sample

---
+++ RabbitFoundation/Rabbit.Web.Mvc/Filters/CompressionFilterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO.Compression;
+using System.Web.Mvc;
+
+namespace Rabbit.Web.Mvc.Filters
+{
+    public class CompressionFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var request = filterContext.HttpContext.Request;
+            var response = filterContext.HttpContext.Response;
+
+            string acceptEncoding = request.Headers["Accept-Encoding"];
+            if (string.IsNullOrEmpty(acceptEncoding))
+            {
+                return;
+            }
+
+            if (acceptEncoding.IndexOf("GZIP", StringComparison.InvariantCultureIgnoreCase) >= 0)
+            {
+                response.AppendHeader("Content-Encoding", "gzip");
+                response.Filter = new GZipStream(response.Filter, CompressionMode.Compress);
+            }
+            else if (acceptEncoding.IndexOf("DEFLATE", StringComparison.InvariantCultureIgnoreCase) >= 0)
+            {
+                response.AppendHeader("Content-Encoding", "deflate");
+                response.Filter = new DeflateStream(response.Filter, CompressionMode.Compress);
+            }
+        }
+    }
+}
---
+++ RabbitFoundation/Rabbit.Web.Mvc/Filters/NoCacheAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Rabbit.Web.Mvc.Filters
+{
+    public class NoCacheAttribute : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            UpdateCachePolicy(filterContext.HttpContext.Response.Cache);
+            base.OnResultExecuting(filterContext);
+        }
+
+        private void UpdateCachePolicy(HttpCachePolicyBase cachePolicy)
+        {
+            cachePolicy.SetNoStore();
+            cachePolicy.SetValidUntilExpires(false);
+            cachePolicy.SetRevalidation(HttpCacheRevalidation.AllCaches);
+            cachePolicy.SetCacheability(HttpCacheability.NoCache);
+        }
+    }
+}
